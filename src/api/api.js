904e2b87cc9519dfa00b1933c1e99fe79db97359
Danae Vogiatzi <danae.vogiatzi@peopleperhour.com>
2021-07-03 17:30:02 +0300

    Add api calls for each page

--- src/api/api.js
+++ src/api/api.js
@@ -2,21 +2,21 @@ import axios from "axios";
 
 export const fetchCategories = async () => {
   const response = await axios.get(
-    "https://bp-interview.herokuapp.com/categories"
+    "/categories"
   );
   return response;
 };
 
 export const fetchCategoryProducts = async (categoryId) => {
   const response = await axios.get(
-    `https://bp-interview.herokuapp.com/categories/${categoryId}`
+    `/categories/${categoryId}/products`
   );
   return response;
 };
 
 export const fetchProduct = async (productId) => {
   const response = await axios.get(
-    `https://bp-interview.herokuapp.com/products/${productId}`
+    `/products/${productId}`
   );
   return response;
 };
--- src/views/listings/Products/Products.js
+++ src/views/listings/Products/Products.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchCategoryProducts } from "../../../api/api";
+import { Link } from "react-router-dom";
 
-const Products = ({categoryId}) => {
-    return (
-        <div>
-            Products
-        </div>
-    );
+const Products = () => {
+  const { id } = useParams();
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    fetchCategoryProducts(id)
+      .catch((err) => console.log(err))
+      .then((response) => {
+        console.log(response);
+        if (!response || response.data.error) setError(response.data.error);
+        setProducts(response.data);
+      });
+  }, [id]);
+  return (
+    <>
+      {error && <div>{error}</div>}
+      <ul>
+        {products &&
+          products.map((product) => (
+            <li key={product.id}>
+              <Link to={`/products/${product.id}`} className="item">
+                {product.title}
+              </Link>
+            </li>
+          ))}
+      </ul>
+    </>
+  );
 };
-
 
 export default Products;
